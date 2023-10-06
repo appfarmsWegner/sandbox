a7a56e5c65c15059d07d1736e36392794e1e7443
Michael Wegner <m.wegner@appfarms.com>
2023-10-06 13:19:22 +0200

    Test

---
+++ Empty/Empty/EmptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyApp.swift
+//  Empty
+//
+//  Created by Michael Wegner on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct EmptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
